1b082c0550cc8b8fef5c736e2f739da1a3edda79
Takahide Nejo <nejoooo@gmail.com>
2024-02-16 23:02:24 -0800

    Add files via upload

---
+++ 02_candidate_AS_events/002_judge_event_positivity_in_each_case.R
@@ -0,0 +1,303 @@
+# Takahide Nejo, MD, PhD 
+# Okada Lab, Department of Neurological Surgery, UC San Francisco
+
+
+# rm all ------------------------------------------------------------------
+
+rm(list = ls(all.names = TRUE)) ;
+
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse) 
+library(tidylog)
+
+
+# dir --------------------------------------------------------------------
+
+dir.1 <- "~/path/to/data"
+dir.2 <- "~/path/to/output_folder"
+
+
+# load data -----------------------------------------------------
+
+setwd(dir.1)
+in.f <- filename # "gtex_9166_n9041_junc_ids_sj_count.tsv"
+sj.count.gtex <- read_tsv(in.f)
+
+setwd(dir.1)
+in.f <- filename # "overlap_table_9041.tsv"
+overlap.table <- read_tsv(in.f)
+
+setwd(dir.1) ; 
+in.f <- filename # "gtex_id_annotation_9166.tsv" ; 
+gtex.annot = read_tsv(in.f)
+
+
+# check -------------------------------------------------------------------
+
+overlap.table %>% dim() %>% print()
+# [1] 9041    2
+
+overlap.table %>% head() %>% print()
+# # A tibble: 6 × 2
+# junc.id                    junc.id.overlap           
+# <chr>                      <chr>                     
+# 1 chr1:+:44086903-44087611   chr1:+:44086903-44087605  
+# 2 chr1:-:94997968-94998645   chr1:-:94998036-94998645  
+# 3 chr1:+:101702655-101707244 chr1:+:101702655-101704377
+# 4 chr1:+:101707328-101707583 NA                        
+# 5 chr1:+:109816292-109816782 chr1:+:109816292-109816643
+# 6 chr1:+:109816292-109817037 chr1:+:109816292-109816643  
+
+
+# define the threshold values -------------------------------------------
+
+THR.C <- 2
+THR.D <- 10
+THR.F <- 0.01
+
+
+# obtain count, depth, freq, and judge -------------------------------------------
+
+for(i in 1:nrow(overlap.table.1)){
+  print(paste0(i, " (", round(i / nrow(overlap.table), 3), ")" ))
+  
+  JUNC.ID <- overlap.table %>% 
+    dplyr::slice(i) %>% 
+    pull(junc.id) ; 
+  JUNC.ID.O <- overlap.table %>% 
+    dplyr::slice(i) %>% 
+    pull(junc.id.overlap) ; 
+  
+  if(is.na(JUNC.ID.O)){
+    res.i <- sj.count.gtex %>% 
+      dplyr::filter(junc.id == JUNC.ID) %>% 
+      gather(case, value, -1) %>% 
+      spread(junc.id, value) %>% 
+      dplyr::rename(count = JUNC.ID) %>% 
+      mutate(count = ifelse(is.na(count), 0, count)) %>% 
+      mutate(depth = count) %>%
+      mutate(freq = round(count / depth, 4)) %>% 
+      mutate(freq = ifelse(is.na(freq), 0, freq)) %>% 
+      mutate(judge = ifelse(count >= THR.C & depth >= THR.D & freq >= THR.F, 1, 0)) %>% 
+      mutate(junc.id = JUNC.ID) %>% 
+      dplyr::select(junc.id, case, count, depth, freq, judge)
+  }else{
+    res.i <- bind_rows(
+      sj.count.gtex %>% dplyr::filter(junc.id == JUNC.ID), 
+      sj.count.gtex %>% dplyr::filter(junc.id == JUNC.ID.O)
+    ) %>% 
+      gather(case, value, -1) %>% 
+      spread(junc.id, value) %>% 
+      dplyr::select(case, count = JUNC.ID, count.o = JUNC.ID.O) %>% 
+      mutate(count = ifelse(is.na(count), 0, count)) %>% 
+      mutate(count.o = ifelse(is.na(count.o), 0, count.o)) %>% 
+      mutate(depth = count + count.o) %>% 
+      mutate(freq = round(count / depth, 4)) %>% 
+      mutate(freq = ifelse(is.na(freq), 0, freq)) %>% 
+      mutate(judge = ifelse(count >= 2 & depth >= 10 & freq >= 0.01, 1, 0)) %>% 
+      mutate(junc.id = JUNC.ID) %>% 
+      dplyr::select(junc.id, case, count, depth, freq, judge)
+  }
+  
+  count.i <- res.i %>% 
+    dplyr::select(junc.id, case, count) %>% 
+    spread(case, count)
+  depth.i <- res.i %>% 
+    dplyr::select(junc.id, case, depth) %>% 
+    spread(case, depth)
+  freq.i <- res.i %>% 
+    dplyr::select(junc.id, case, freq) %>% 
+    spread(case, freq)
+  judge.i <- res.i %>% 
+    dplyr::select(junc.id, case, judge) %>% 
+    spread(case, judge)
+  
+  if(i == 1){
+    count <- count.i 
+    depth <- depth.i 
+    freq <- freq.i 
+    judge <- judge.i
+  }else{
+    count <- bind_rows(count, count.i) 
+    depth <- bind_rows(depth, depth.i)
+    freq <- bind_rows(freq, freq.i) 
+    judge <- bind_rows(judge, judge.i) 
+  }
+}
+
+
+# check -------------------------------------------------------------------
+
+count %>% dim() %>% print() 
+depth %>% dim() %>% print() 
+freq %>% dim() %>% print() 
+judge %>% dim() %>% print() 
+# [1]  9041 9167
+
+count %>% anyNA() %>% print() 
+depth %>% anyNA() %>% print() 
+freq %>% anyNA() %>% print() 
+judge %>% anyNA() %>% print() 
+# [1] FALSE
+
+
+# out ---------------------------------------------------------------------
+
+setwd(dir.2)
+# count
+out.f <- filename # "gtex_9166_count.tsv"
+write_tsv(count, out.f)
+# depth
+out.f <- filename # "gtex_9166_depth.tsv"
+write_tsv(depth, out.f)
+# freq
+out.f <- filename # "gtex_9166_freq.tsv"
+write_tsv(freq, out.f)
+# judge
+out.f <- filename # "gtex_9166_judge.tsv"
+write_tsv(judge, out.f)
+
+
+# prep 3 subgroups --------------------------------------------------------------------
+
+cases <- list(
+  gtex.annot %>% pull(sample.id), 
+  gtex.annot %>% dplyr::filter(cls != "Testis") %>% pull(sample.id), 
+  gtex.annot %>% dplyr::filter(cls == "Brain") %>% pull(sample.id)
+  ) ;
+names(cases) <- c("GTEx_all", "GTEx_all_but_ts", "Brain") ;
+
+
+# calculate psr -----------------------------------------------------------
+
+for(i in 1:3){
+  print(i)
+  CASES <- cases[[i]]
+  
+  judge.i <- judge %>% 
+    dplyr::select(junc.id, colnames(judge)[is.element(colnames(judge), CASES)])
+  
+  psr.i <- judge.i %>% 
+    mutate(pos = apply(judge.i[, -1], 1, sum)) %>% 
+    mutate(psr = round(pos / length(CASES), 4)) %>% 
+    dplyr::select(junc.id, psr)
+
+  colnames(psr.i)[2] <- names(cases)[i]
+  
+  if(i == 1){
+    res.psr.new <- psr.i
+  }else{
+    res.psr.new <- res.psr.new %>% 
+      bind_cols(psr.i %>% dplyr::select(- junc.id) )
+  }
+}
+
+
+# check -------------------------------------------------------------------
+
+res.psr.new %>% nrow() %>% print()
+# [1] 9041
+
+
+# out ---------------------------------------------------------------------
+
+setwd(dir.2)
+out.f <- filename # "gtex_9166_psr.tsv")
+write_tsv(res.psr.new, out.f)
+
+
+# si ----------------------------------------------------------------------
+
+Sys.time() %>% print()
+# 
+
+sessionInfo() %>% print()
+# R version 4.1.3 (2022-03-10)
+# Platform: x86_64-pc-linux-gnu (64-bit)
+# Running under: CentOS Linux 7 (Core)
+# 
+# Matrix products: default
+# BLAS:   /software/c4/cbi/software/R-4.1.3-gcc8/lib64/R/lib/libRblas.so
+# LAPACK: /software/c4/cbi/software/R-4.1.3-gcc8/lib64/R/lib/libRlapack.so
+# 
+# locale:
+# [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
+# [3] LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
+# [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
+# [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+# [9] LC_ADDRESS=C               LC_TELEPHONE=C            
+# [11] LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+# 
+# attached base packages:
+# [1] stats4    parallel  stats     graphics  grDevices utils     datasets 
+# [8] methods   base     
+# 
+# other attached packages:
+# [1] ggsci_2.9                         readxl_1.3.1                     
+# [3] AnnotationHub_3.0.2               BiocFileCache_2.0.0              
+# [5] dbplyr_2.1.1                      Rsamtools_2.8.0                  
+# [7] BSgenome.Hsapiens.UCSC.hg19_1.4.3 BSgenome_1.60.0                  
+# [9] rtracklayer_1.52.1                Biostrings_2.60.2                
+# [11] XVector_0.32.0                    EnsDb.Hsapiens.v75_2.99.0        
+# [13] ensembldb_2.16.4                  AnnotationFilter_1.16.0          
+# [15] GenomicFeatures_1.44.2            AnnotationDbi_1.54.1             
+# [17] Biobase_2.52.0                    GenomicRanges_1.44.0             
+# [19] GenomeInfoDb_1.28.1               IRanges_2.26.0                   
+# [21] S4Vectors_0.30.2                  BiocGenerics_0.38.0              
+# [23] tidylog_1.0.2                     forcats_0.5.1                    
+# [25] stringr_1.4.0                     dplyr_1.0.9                      
+# [27] purrr_0.3.4                       readr_2.1.2                      
+# [29] tidyr_1.2.0                       tibble_3.1.7                     
+# [31] ggplot2_3.3.6                     tidyverse_1.3.1                  
+# 
+# loaded via a namespace (and not attached):
+# [1] colorspace_2.0-3              rjson_0.2.20                 
+# [3] ellipsis_0.3.2                fs_1.5.2                     
+# [5] rstudioapi_0.13               bit64_4.0.5                  
+# [7] interactiveDisplayBase_1.30.0 fansi_1.0.3                  
+# [9] lubridate_1.9.2               xml2_1.3.2                   
+# [11] cachem_1.0.6                  jsonlite_1.8.0               
+# [13] broom_0.7.9                   png_0.1-7                    
+# [15] shiny_1.7.1                   BiocManager_1.30.16          
+# [17] compiler_4.1.3                httr_1.4.3                   
+# [19] backports_1.2.1               assertthat_0.2.1             
+# [21] Matrix_1.6-0                  fastmap_1.1.0                
+# [23] lazyeval_0.2.2                cli_3.6.1                    
+# [25] later_1.3.0                   htmltools_0.5.5              
+# [27] prettyunits_1.1.1             tools_4.1.3                  
+# [29] gtable_0.3.0                  glue_1.6.2                   
+# [31] GenomeInfoDbData_1.2.6        rappdirs_0.3.3               
+# [33] Rcpp_1.0.8.3                  cellranger_1.1.0             
+# [35] vctrs_0.6.1                   rvest_1.0.1                  
+# [37] mime_0.12                     timechange_0.2.0             
+# [39] lifecycle_1.0.3               restfulr_0.0.13              
+# [41] XML_3.99-0.7                  zlibbioc_1.38.0              
+# [43] scales_1.2.0                  vroom_1.5.7                  
+# [45] promises_1.2.0.1              clisymbols_1.2.0             
+# [47] hms_1.1.0                     MatrixGenerics_1.4.3         
+# [49] ProtGenerics_1.25.2           SummarizedExperiment_1.22.0  
+# [51] yaml_2.3.5                    curl_4.3.2                   
+# [53] memoise_2.0.0                 biomaRt_2.48.3               
+# [55] stringi_1.7.6                 RSQLite_2.2.8                
+# [57] BiocVersion_3.13.1            BiocIO_1.2.0                 
+# [59] filelock_1.0.2                BiocParallel_1.26.2          
+# [61] rlang_1.1.0                   pkgconfig_2.0.3              
+# [63] bitops_1.0-7                  matrixStats_0.62.0           
+# [65] lattice_0.20-44               GenomicAlignments_1.28.0     
+# [67] bit_4.0.4                     tidyselect_1.1.2             
+# [69] magrittr_2.0.3                R6_2.5.1                     
+# [71] generics_0.1.2                DelayedArray_0.18.0          
+# [73] DBI_1.1.2                     pillar_1.7.0                 
+# [75] haven_2.4.3                   withr_2.5.0                  
+# [77] KEGGREST_1.32.0               RCurl_1.98-1.3               
+# [79] modelr_0.1.8                  crayon_1.5.1                 
+# [81] utf8_1.2.2                    tzdb_0.1.2                   
+# [83] progress_1.2.2                grid_4.1.3                   
+# [85] blob_1.2.2                    reprex_2.0.1                 
+# [87] digest_0.6.29                 xtable_1.8-4                 
+# [89] httpuv_1.6.5                  munsell_0.5.0         
+
+
+# end ---------------------------------------------------------------------
